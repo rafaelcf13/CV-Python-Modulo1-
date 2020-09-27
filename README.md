98e3019d22de5606b6329967464b5cb0e858faf5
rafaelcf13 <cf_rafael@yahoo.com.br>
2020-09-26 23:43:52 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Exercicios-python
+ Exercicios basicos de Python - Curso em video
---
+++ ex014.py
@@ -0,0 +1,3 @@
+c = float(input('Informe a temperatura em ºCelsius: '))
+f = 9 * c / 5 + 32
+print('A temperatura {}ºCelsius equivale a {} em Fahrenheit'.format(c,f))
